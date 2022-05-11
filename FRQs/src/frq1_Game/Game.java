11067ecda02675ac3eae69f09ce43fce396dea6c
s-trao <s-trao@484M114512.lwsd.k12wa.org>
2022-05-11 12:40:02 -0700

    Fixing the Game. java file

--- FRQs/src/frq1_Game/Game.java
+++ FRQs/src/frq1_Game/Game.java
@@ -41,19 +41,18 @@ public class Game {
         int score = 0;
         boolean done = levelOne.goalReached();
 
-        if (!done) {
+        if (done) {
             score += levelOne.getPoints();
-            done = levelTwo.goalReached();
+            done = done && levelTwo.goalReached();
         }
-        if (!done) {
+        if (done) {
             score += levelTwo.getPoints();
-            done = levelThree.goalReached();
+            done = done && levelThree.goalReached();
         }
-        if (!done) {
+        if (done) {
             score += levelThree.getPoints();
-            done = isBonus();
         }
-        if (!done) {
+        if (isBonus()) {
             score *= 3;
         }
         
@@ -67,7 +66,7 @@ public class Game {
     public int playManyTimes(int num) {
         
         /* IMPLEMENTATION OF PART (B) */
-        int max = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
         for (int i = 0; i < num; i++) {
             play();
             max = Math.max(max, getScore());
